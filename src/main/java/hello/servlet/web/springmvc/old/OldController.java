7287a4eac02fbfce50cfca186d58498aa214e0e3
HT <yangheetae0827@naver.com>
2021-07-28 17:22:39 +0900

    - SpringMvc v1,v2

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") //springBean의 이름이 url로 된다.
+public class OldController implements Controller {
+
+    // SimpleControllerHandlerAdapter
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
+
+
